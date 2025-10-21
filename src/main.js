89044e8cbbf19e18de2ebcbea651cac9f2ca030c
sfdeloach <sfdeloach@gmail.com>
2025-10-20 22:00:24 -0400

    img fix, favicon add

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <img class="photo" src="/public/teddy_number_1.jpeg" alt="Teddy is better than Cookie." />
+    <img class="photo" src="images/teddy_number_1.jpeg" alt="Teddy is better than Cookie." />
     <h1>Welcome to my website!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p>
-      Exciting things are here!
+      <pre>if(cookie < teddy) // infinite loop</pre>
     </p>
   </div>
 `;
